3d0a89f1ab5dd2261e4d5768238b6db2866bf9ad
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2022-03-24 12:59:11 +0100

    data

--- Python/ReadAndFormatData/readData.py
+++ Python/ReadAndFormatData/readData.py
@@ -1,5 +1,12 @@
-import json;
+import csv;
 
-fileName = ""
-openedFile = open(fileName)
+fileName = "C:\Code\Python\MalarenergiAI\Python\Data\Data_Hallsta_tvättad.csv"
+writeFileName = "Data_Hallsta_torktumlad.csv"
+
+with open(fileName, mode='r') as file:
+    csvFile = csv.reader(file)
+ 
+    for lines in csvFile:
+        print(lines)
+    
 
