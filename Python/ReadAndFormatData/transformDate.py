8b29e58c48e46c14850bef0b261b60d6f1117c5d
abbmatlan <54663306+abbmatlan@users.noreply.github.com>
2022-03-24 15:24:35 +0100

    Create transformDate.py

---
+++ Python/ReadAndFormatData/transformDate.py
@@ -0,0 +1,68 @@
+import csv
+
+fileName = "C:\Development\Malarenergi AI-projekt\Github\Python\Data\Data_Hallsta_torktumlad.csv"
+writeFileName = "Data_Hallsta_torktumlad.csv"
+
+with open(fileName, mode='r') as file:
+    csvFile = csv.reader(file)
+    columns = list(csvFile)
+
+for i in range(0, len(columns)):
+        head, sep, tail = columns[i][0].partition(' ')
+        
+        #delar upp år, månad och dag 
+        year, middle, dayMonth = head.partition('-')
+        month, middle2, day = dayMonth.partition('-')
+
+        #delar upp timme och minut
+        hour, middle3, minSec = tail.partition(':')
+        
+        hour = int(hour)
+        day = int(day)
+        month = int(month)
+        year = int(year)
+
+        totalHour = 0
+
+
+        if month >= 1:
+            totalHour += (day*24 + hour) 
+
+        if month >= 2 :
+            totalHour += ((31)*24) 
+
+        if month >= 3:
+            if int(year%4) == 0:
+                totalHour += ((29)*24) 
+            else:
+                totalHour += ((28)*24)
+        
+        if month >= 4:
+            totalHour += ((31)*24)
+
+        if month >= 5:
+            totalHour += ((30)*24)
+
+        if month >= 6:
+            totalHour += ((31)*24) 
+
+        if month >= 7:
+            totalHour += ((30)*24) 
+
+        if month >= 8:
+            totalHour += ((31)*24)
+
+        if month >= 9:
+            totalHour += ((31)*24) 
+
+        if month >= 10:
+            totalHour += ((30)*24)
+
+        if month >= 11:
+            totalHour += ((31)*24)
+
+        if month >= 12:
+            totalHour += ((30)*24) 
+
+        print(totalHour)
+    
