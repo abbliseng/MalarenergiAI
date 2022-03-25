aea285a0ef07ca5276cf5f3593593e666896a831
abbmatlan <54663306+abbmatlan@users.noreply.github.com>
2022-03-25 10:38:39 +0100

    Update transformDate.py

--- Python/ReadAndFormatData/transformDate.py
+++ Python/ReadAndFormatData/transformDate.py
@@ -42,6 +42,25 @@ def calculate_hour (hour,day,month,year):
 
     return (totalHour)
 
+rawData = csv.reader(open(fileName), delimiter=';')
+newFile = csv.writer(open(writeFileName, "w", newline=''), delimiter=';')
+
+for row in rawData:
+
+    # for i in range(0, len(columns)):
+    head, sep, tail = row[0].partition(' ')
+
+    # delar upp år, månad och dag
+    year, middle, dayMonth = head.partition('-')
+    month, middle2, day = dayMonth.partition('-')
+
+    # delar upp timme och minut
+    hour, middle3, minSec = tail.partition(':')
+
+    totalHour = calculate_hour (hour,day,month,year)
+    row.append(totalHour)
+    newFile.writerow(row)
+
 # for i in range(0, len(columns)):
 #     #delar upp basdatat
 #     head, sep, tail = columns[i][0].partition(' ')
@@ -83,77 +102,3 @@ except:
     print("Enter a valid integer of an existing date. Exampel: Year=2022, Month=03, Day=20, Hour=13. The hours are a inteval from 0 to 23")
 
 
-
-
-rawData = csv.reader(open(fileName), delimiter=';')
-
-# with open(writeFileName, mode='w', newline='') as wfile:
-
-newFile = csv.writer(open(writeFileName, "w", newline=''), delimiter=';')
-for row in rawData:
-
-    # for i in range(0, len(columns)):
-    head, sep, tail = row[0].partition(' ')
-
-    # delar upp år, månad och dag
-    year, middle, dayMonth = head.partition('-')
-    month, middle2, day = dayMonth.partition('-')
-
-    # delar upp timme och minut
-    hour, middle3, minSec = tail.partition(':')
-
-    hour = int(hour)
-    day = int(day)
-    month = int(month)
-    year = int(year)
-
-    totalHour = 0
-
-    if month >= 1:
-        totalHour += (day*24 + hour)
-
-    if month >= 2:
-        totalHour += ((31)*24)
-
-    if month >= 3:
-        if int(year % 4) == 0:
-            totalHour += ((29)*24)
-        else:
-            totalHour += ((28)*24)
-
-    if month >= 4:
-        totalHour += ((31)*24)
-
-    if month >= 5:
-        totalHour += ((30)*24)
-
-    if month >= 6:
-        totalHour += ((31)*24)
-
-    if month >= 7:
-        totalHour += ((30)*24)
-
-    if month >= 8:
-        totalHour += ((31)*24)
-
-    if month >= 9:
-        totalHour += ((31)*24)
-
-    if month >= 10:
-        totalHour += ((30)*24)
-
-    if month >= 11:
-        totalHour += ((31)*24)
-
-    if month >= 12:
-        totalHour += ((30)*24)
-
-    row.append(totalHour)
-    newFile.writerow(row)
-
-    # print(totalHour)
-    # writer = csv.writer(wfile, delimiter=',')
-    # columns[i].append(totalHour)
-    # writer.writerow(columns[i])
-
-    # wfile.write(str(columns[i]))
