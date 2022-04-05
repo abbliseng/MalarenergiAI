139083f0df4b6fffe70b98f4927a17bc335ba1e5
Lisa Engström <Lisa.Engstrom@abbgymnasiet.se>
2022-04-05 15:04:44 +0200

    soltimmar :)

--- Python/ReadAndFormatData/soltimmar.py
+++ Python/ReadAndFormatData/soltimmar.py
@@ -9,45 +9,38 @@ col_names = [
 
 df = pd.read_csv("./Data/smhi_opendata_soltimmar.csv", names=col_names, delimiter=";", index_col ="datum")
 df = df.iloc[10: , :]
+
 col_names2 = [
              'år',
              'månad',
              'dag',
              'timme',
-             'effekt'
+             'temp',
+             'effekt',
+             'vind'
 ]
-
 df2 = pd.read_csv("./Data/Data_Hallsta_test.csv", names=col_names2, delimiter=";")
-valid_dates = []
+
+datapoints = []
+
 for i in range(len(df2)):
-    day = str(int(df2.iloc[i]['dag']))
-    ar = str(int(df2.iloc[i]['år']))
-    manad = str(int(df2.iloc[i]['månad']))
-    if len(day) <= 1:
-        day = '0'+day
-    if len(manad) <= 1:
-        manad = '0'+manad
+    år = str(int(df2.iloc[i]['år']))
+    månad = str(int(df2.iloc[i]['månad']))
+    dag = str(int(df2.iloc[i]['dag']))
+    timme = str(int(df2.iloc[i]['timme']))
+    if (len(månad)<2):
+        månad = '0'+månad
+    if (len(dag)<2):
+        dag = '0'+dag
+    if (len(timme)<2):
+        timme = '0'+timme
+    timme += ':00:00'
+    date = år+'-'+månad+'-'+dag
+    for v in df.loc[date].values:
+        if (v[0] == timme):
+            datapoints.append(v[1])
         
-    date = ar+'-'+manad+'-'+day
+print(len(datapoints))
+df2['soltimmar'] = datapoints
 
-    print(df.loc[date].values)
-
-    #valid_dates.append(df.loc[date])
-#print(valid_dates)
-
-"""
-start_index = 0
-end_index = 0
-for i in range(len(df)):
-    if (i%1000 == 0):
-        print("{0:.0%} complete.".format(i / len(df)))
-        
-    if (df.iloc[i]['datum'] == '2016-10-11' and df.iloc[i]['tid'] == '07:00:00'):
-        start_index = i
-    elif (df.iloc[i]['datum'] == '2019-02-27' and df.iloc[i]['tid'] == '23:00:00'):
-        end_index = i
-
-df = df.iloc[start_index:end_index , :]
-
-print('Length :',end_index-start_index,':',len(df))
-"""
+df2.to_csv('./Data/Data_Hallsta_vikt.csv', index=False , sep=";", header=False)
