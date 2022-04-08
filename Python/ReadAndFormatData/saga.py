378b8c5a3fb34eb9736fc1c556aa1d68d3de5cba
Lisa Engström <Lisa.Engstrom@abbgymnasiet.se>
2022-04-08 17:30:23 +0200

    organisering + konstigheter
    lista ut vart datan byter och dela upp allt i mappar.

--- Python/ReadAndFormatData/saga.py
+++ Python/ReadAndFormatData/saga.py
@@ -1,17 +1,29 @@
 import csv
 
 fileName = "./Data/Data_Hallsta_test.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_test.csv"
-newFileName = "./Data/Data_Hallsta_Kallt.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+
+newFileName1 = "./Data/Data_Hallsta_2016.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+newFileName2 = "./Data/Data_Hallsta_2017.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+newFileName3 = "./Data/Data_Hallsta_2018.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+newFileName4 = "./Data/Data_Hallsta_2019.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
 
 
 rawData = csv.reader(open(fileName), delimiter=';')
-newFile = csv.writer(open(newFileName, "w", newline=''), delimiter=';')
+newFile1 = csv.writer(open(newFileName1, "w", newline=''), delimiter=';')
+newFile2 = csv.writer(open(newFileName2, "w", newline=''), delimiter=';')
+newFile3 = csv.writer(open(newFileName3, "w", newline=''), delimiter=';')
+newFile4 = csv.writer(open(newFileName4, "w", newline=''), delimiter=';')
 # newData = []
 
 for row in rawData:
-    #print(row[1])
-    if row[1] == 1 or 2 or 3 or 11 or 12:
-        # newData.append(row)
-        newFile.writerow(row)
+    #print(row[0])
+    if row[0] == "2016":
+        newFile1.writerow(row)
+    elif row[0] == "2017":
+        newFile2.writerow(row)
+    elif row[0] == "2018":
+        newFile3.writerow(row)
+    elif row[0] == "2019":
+        newFile4.writerow(row)
 
 # newFile.writerow()
