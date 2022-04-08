a1c3b6488901a1c34ff8b5b16e8fab66c1aad5f4
Lisa Engström <Lisa.Engstrom@abbgymnasiet.se>
2022-04-08 17:43:53 +0200

    uppdelad

--- Python/ReadAndFormatData/saga.py
+++ Python/ReadAndFormatData/saga.py
@@ -1,29 +1,36 @@
 import csv
 
-fileName = "./Data/Data_Hallsta_test.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_test.csv"
+fileName = "./Data/Hallsta/Data_Hallsta_test.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_test.csv"
 
-newFileName1 = "./Data/Data_Hallsta_2016.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
-newFileName2 = "./Data/Data_Hallsta_2017.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
-newFileName3 = "./Data/Data_Hallsta_2018.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
-newFileName4 = "./Data/Data_Hallsta_2019.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+newFileName1 = "./Data/Data_Hallsta_2016-18.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+newFileName2 = "./Data/Data_Hallsta_2018-19.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+#newFileName3 = "./Data/Data_Hallsta_2018.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
+#newFileName4 = "./Data/Data_Hallsta_2019.csv" #"C:\\Users\\s9sagsel\\Desktop\\Programmering\\MälarenergiAI\\Data_Hallsta_Kallt.csv"
 
 
 rawData = csv.reader(open(fileName), delimiter=';')
 newFile1 = csv.writer(open(newFileName1, "w", newline=''), delimiter=';')
 newFile2 = csv.writer(open(newFileName2, "w", newline=''), delimiter=';')
-newFile3 = csv.writer(open(newFileName3, "w", newline=''), delimiter=';')
-newFile4 = csv.writer(open(newFileName4, "w", newline=''), delimiter=';')
+#newFile3 = csv.writer(open(newFileName3, "w", newline=''), delimiter=';')
+#newFile4 = csv.writer(open(newFileName4, "w", newline=''), delimiter=';')
 # newData = []
 
+change_point = ["2018", "10", "23", "15"]
+before = True
+
 for row in rawData:
-    #print(row[0])
-    if row[0] == "2016":
+    if row[:4] == change_point:
+        before = False
+    if before:
         newFile1.writerow(row)
-    elif row[0] == "2017":
+    else:
         newFile2.writerow(row)
-    elif row[0] == "2018":
-        newFile3.writerow(row)
-    elif row[0] == "2019":
-        newFile4.writerow(row)
+    #print(row[:4])
+    #if (row[0] == change_point[0] and row[1] == change_point[1] and )
+
+    #if row[0] == "2018":
+    #    newFile3.writerow(row)
+    #elif row[0] == "2019":
+    #    newFile4.writerow(row)
 
 # newFile.writerow()
